4fba9478f07b9872e8d5aa25926cc751e3574e16
Koichi Nakayamada <koichi.nakayamada@gmail.com>
2024-12-16 18:40:57 -0800

    Start off learning basics of Python

---
+++ free-code-camp/numbers.py
@@ -0,0 +1,14 @@
+from math import *
+
+print(-2.0987)
+print(3 * (4 + 5))
+print(10 % 3)
+
+my_num = 5
+print(abs(my_num))
+print(pow(3, 6))
+print(max(4, 6, 5))
+print(round(3.7))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
