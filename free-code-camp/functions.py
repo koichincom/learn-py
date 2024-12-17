3cca4c5c269f242d85243f19c35140053a0cb878
Koichi Nakayamada <koichi.nakayamada@gmail.com>
2024-12-17 12:09:22 -0800

    commit

---
+++ free-code-camp/functions.py
@@ -0,0 +1,7 @@
+def say_hi(name, age):
+    print(f"Hello, {name}!")
+    print(f"You are {age} years old.")
+
+user = input("What is your name? ")
+old = input("How old are you? ")
+say_hi(user, old)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Resources I used
+
+- [free-code-camp: https://www.youtube.com/watch?v=rfscVS0vtbw](https://www.youtube.com/watch?v=rfscVS0vtbw)
