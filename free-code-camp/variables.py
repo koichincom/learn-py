01bb9c3b8e3c6873a03bc51327904b2f25ffb326
Koichi Nakayamada <koichi.nakayamada@gmail.com>
2024-12-17 00:46:38 -0800

    commit

--- free-code-camp/variables.py
+++ free-code-camp/variables.py
@@ -2,4 +2,4 @@ character_name = "John"
 character_age = 35
 is_male = False
 
-print(f"There once was a man named {character_name}, and he's {character_age} years old.")
+print(f"There was a man named {character_name}, and he's {character_age} years old.")
