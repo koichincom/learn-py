1b64039af9d3f9ebad7decd407baa0064ebbb6eb
Koichi Nakayamada <koichi.nakayamada@gmail.com>
2025-01-02 15:27:02 -0800

    Commit Message

---
+++ cs50_python/compare.py
@@ -0,0 +1,16 @@
+x = int(input("x: "))
+y = int(input("y: "))
+
+# if x < y:
+#     print("x is less than y")
+# elif x > y:
+#     print("x is greater than y")
+# else:
+#     print("x is equal to y")
+
+# Using elif and else for efficiency
+
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y") 
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 - [x] [free_code_camp](https://www.youtube.com/watch?v=rfscVS0vtbw)
 - [ ] [cs50_python](https://www.youtube.com/watch?v=nLRL_NcnK-4)
 
-progress: 1:51:00
+progress: 2:26:12
