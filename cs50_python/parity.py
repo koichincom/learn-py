01f4faa4993fa3f9b8e2ffbcac198309656985bb
Koichi Nakayamada <koichi.nakayamada@gmail.com>
2025-01-11 23:02:56 -0800

    Commit Message

--- cs50_python/parity.py
+++ cs50_python/parity.py
@@ -1,6 +1,25 @@
-num = int(input("Number: "))
+# num = int(input("Number: "))
+#
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
 
-if num % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+def main():
+    x = int(input("Number: "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    # if n % 2 == 0:
+    #     return True
+    # else:
+    #     return False
+    # Turn these 4 lines into 1 line
+
+    # return True if n % 2 == 0 else False
+    return n % 2 == 0
+
+main()
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 - [x] [free_code_camp](https://www.youtube.com/watch?v=rfscVS0vtbw)
 - [ ] [cs50_python](https://www.youtube.com/watch?v=nLRL_NcnK-4)
 
-progress: 2:26:12
+progress: 3:10:52/15:57:47
